4b412cfb8feccf46ed159c21047bf52b3e821411
Yash Handa <yashhanda7@yahoo.com>
2018-07-03 12:37:48 +0530

    Initial Commit

---
+++ aboutContent.js
@@ -0,0 +1,20 @@
+const content = {
+  title: 'About Page',
+  topic: 'Family List',
+  lastName: 'Handa',
+  members: [{
+    name: 'Rakesh',
+    age: 43
+  }, {
+    name: 'Pooja',
+    age: 39
+  }, {
+    name: 'Yash',
+    age: 19
+  }, {
+    name: 'Tanveesha',
+    age: 14
+  }]
+};
+
+module.exports = content;
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const content = require('./aboutContent');
+
+let app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+hbs.registerHelper('ageStatus', (age) => {
+  if (age >= 65) return '(Senior)';
+  else if (age >= 18) return '(Adult)';
+  else return '(Minor)';
+});
+
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  const log = `${now}:  method: ${req.method}, route: ${req.url}`
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) console.log('The log could not be appended to the server.log file');
+  });
+  console.log(log);
+  next()
+});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     title: 'Maintenance'
+//   });
+// });
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (request, response) => {
+  //response.send('<h1>My first Server</h1>');
+  response.render('home.hbs', {
+    title: 'Home Page',
+    welcomeMessage: 'Hello EveryBody!! <br>Have Fun',
+  });
+});
+
+app.get('/about', (request, response) => {
+  //response.send('<h2>About Page</h2>');
+  response.render('about.hbs', content);
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'Unable to manage request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('The server is up and running');
+});
